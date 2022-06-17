c139cea8b88f2edae97da45857fffd0902a962af
Christian Coleman <107517907+Hingle27@users.noreply.github.com>
2022-06-17 14:02:29 -0400

    Finally! All hail the Donut!!
    I found where my error occurred. Program kept crashing because of an indent needed at the end pygame.display.update(), needed to be in the "For i" loop above it.

--- Donut.py
+++ Donut.py
@@ -1,12 +1,17 @@
 import pygame
-pygame.font.init()
 import math
-pygame.init
+#added colorsys import
+import colorsys
+
+pygame.init()
 
 white = (255, 255, 255)
 black = (0, 0, 0)
+hue = 0
+
+
 WIDTH = 1000
-HEIGHT = 500
+HEIGHT = 750
 
 x_start, y_start = 0,0
 x_seperator = 10
@@ -20,7 +25,7 @@ y_offset = rows / 2
 A, B = 0, 0
 theta_spacing = 10  
 phi_spacing = 1
-chars = '!@##$$%^^&&*'
+chars = '.,-~:;=!*#$@'
 
 
 
@@ -29,7 +34,7 @@ display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #display_surface = pygame.display.set_mode((0, 0)), pygame.FULLSCREEN
 pygame.display.set_caption('Donut')
-font = pygame.font.SysFont('Arial', 18, bold = True)
+font = pygame.font.SysFont('Arial', 18, bold=True)
 
 def text_display(letter, x_start, y_start):
     text = font.render(str(letter), True, white)
@@ -37,11 +42,10 @@ def text_display(letter, x_start, y_start):
 
 
 run = True
-
 while run:
     screen.fill((black))
     z = [0] * screen_size
-    b = [''] * screen_size
+    b = [' '] * screen_size
 
     for j in range(0, 628, theta_spacing):
         for i in range(0, 628, phi_spacing):
@@ -51,8 +55,8 @@ while run:
             f = math.sin(j)
             g = math.cos(A)
             h = d + 2
+            D = 1 / (c * h * e + f * g + 5)
             l = math.cos(i)
-            D = l / (c * h * e + f * g + 5)
             m = math.cos(B)
             n = math.sin(B)
             t = c * h * g - f * e
@@ -63,27 +67,27 @@ while run:
             if rows > y and y > 0 and x > 0 and columns > x and D > z[o]:
                 z[o] = D
                 b[o] = chars[N if N > 0 else 0]
+    
+    
     if y_start == rows * y_seperator - y_seperator:
         y_start = 0
-        for i in range(len(b)):
-            A += 0.000002
-            B += 0.000001
-            if i == 0 or i % columns:
-                text_display(b[i], x_start, y_start)
-                x_start += x_seperator
-            else:
-                y_start += y_seperator 
-                x_start = 0
-                text_display(b[i], x_start, y_start)
-                x_start += x_seperator 
 
 
+    for i in range(len(b)):
+        A += 0.000002
+        B += 0.000001
+        if i == 0 or i % columns:
+            text_display(b[i], x_start, y_start)
+            x_start += x_seperator
+        else:
+            y_start += y_seperator 
+            x_start = 0
+            text_display(b[i], x_start, y_start)
+            x_start += x_seperator 
 
+    pygame.display.update()
 
-
-pygame.display.update()
-
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-     run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
